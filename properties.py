b441e3555f6601e45aa4281997a8b751c13edb63
MWY3510 <gordon.sandra@sbcglobal.net>
2015-07-19 22:42:49 -0400

    Multires display driver.
    Adding support for multires display driver and later windows and x11.

--- properties.py
+++ properties.py
@@ -25,6 +25,7 @@
 
 import bpy
 import os
+#import sys
 import xml.etree.ElementTree as ET
 #from .properties_shader import RendermanCoshader, coshaderShaders
 
@@ -516,7 +517,12 @@ class RendermanSceneSettings(bpy.types.PropertyGroup):
         items = [('openexr', 'OpenEXR', 'Render to a OpenEXR file, to be read back into Blender\'s Render Result'),
                     ('tiff', 'Tiff', 'Render to a TIFF file, to be read back into Blender\'s Render Result'),
                     ('png', 'PNG', 'Render to a PNG file, to be read back into Blender\'s Render Result'),
-                    ('it', 'it', 'External framebuffer display (must have RMS installed)')]
+                    ('it', 'it', 'External framebuffer display (must have RMS installed)'),
+                    ('multires', 'multires', 'External render window that has high performance for re-render.')]
+        #if sys.platform == ("win32"):
+        #    items.append(('windows', 'windows', 'Display window for windows q for quite and s for save.'))
+        #else:
+        #    items.append(('x11', 'x11', 'Display window for linux systems q for quite and s for save.'))
         return items
         
     display_driver = EnumProperty(
