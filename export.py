29b412fbc7bd4f80f8bfcb78b333cc4fd23cc04d
bsavery <brian.savery@gmail.com>
2015-08-20 19:09:54 -0700

    Fix light linking

--- export.py
+++ export.py
@@ -1615,6 +1615,11 @@ def export_object_attributes(ri, ob):
 
         ri.Attribute("trace", trace_params)
 
+    # light linking
+    for link in ob.renderman.light_linking:
+        if link.illuminate != "DEFAULT":
+            ri.Illuminate(link.light, link.illuminate == 'ON')
+
 
 # for each mat in this mesh, call it, then do some shading wizardry to
 # switch between them with PxrBxdfBlend
