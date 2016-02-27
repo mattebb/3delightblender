e0c969d83f179285b7c3b6dfdcc2eaa9d169af5e
MWY3510 <rgordon@cedarville.edu>
2016-02-27 01:00:08 -0500

    Scene menu is back
    Added back in the scene menu to be able to adjust the scene's units and
    also physics constants.

--- ui.py
+++ ui.py
@@ -54,6 +54,12 @@ properties_material.MATERIAL_PT_context_material.COMPAT_ENGINES.add(
 properties_material.MATERIAL_PT_custom_props.COMPAT_ENGINES.add('PRMAN_RENDER')
 del properties_material
 
+import bl_ui.properties_scene as properties_scene
+properties_scene.SCENE_PT_scene.COMPAT_ENGINES.add('PRMAN_RENDER')
+properties_scene.SCENE_PT_unit.COMPAT_ENGINES.add('PRMAN_RENDER')
+properties_scene.SCENE_PT_physics.COMPAT_ENGINES.add('PRMAN_RENDER')
+del properties_scene
+
 import bl_ui.properties_data_lamp as properties_data_lamp
 properties_data_lamp.DATA_PT_context_lamp.COMPAT_ENGINES.add('PRMAN_RENDER')
 properties_data_lamp.DATA_PT_spot.COMPAT_ENGINES.add('PRMAN_RENDER')
@@ -921,7 +927,7 @@ class OBJECT_PT_renderman_object_geometry(Panel):
 
             
         col = layout.column()
-        col.operator("object.export_rib_archive", icon="EXPORT", text="Export Object as RIB Archive.")
+        col.operator("export.export_rib_archive", icon="EXPORT", text="Export Object as RIB Archive.")
         
         
         
