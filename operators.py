3d43be06bc197186a09e196138e93e5fcd549607
MWY3510 <rgordon@cedarville.edu>
2016-05-05 10:52:40 -0400

    Don't enable examples unless in PRMan mode.
    I forgot to limit the example menu to just be active when renderman is selected.

--- operators.py
+++ operators.py
@@ -457,6 +457,8 @@ class LoadSceneMenu(bpy.types.Menu):
 
 
 def menu_draw(self, context):
+    if context.scene.render.engine != "PRMAN_RENDER":
+            return
     icons = load_icons()
     examples_menu = icons.get("help")
     self.layout.menu("examples", icon_value=examples_menu.icon_id)
