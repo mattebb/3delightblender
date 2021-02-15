02af038d3a16a004affdef5f25ba3ed01a757cf2
Ian Hsieh <16354042+ian-hsieh@users.noreply.github.com>
2021-02-15 13:43:22 -0800

    We now default to rman's filmic-blender OCIO config file, if the OCIO environment variable is not set.

--- rfb_utils/color_manager_blender.py
+++ rfb_utils/color_manager_blender.py
@@ -40,8 +40,6 @@ def get_config_path():
 
     ociopath = os.getenv('OCIO', None)
     if ociopath is None:
-        if 'RFB_USE_BLENDER_OCIO' not in os.environ:
-            return ''
         ociopath = get_blender_ocio_config()
 
     if ColorManager:
@@ -57,8 +55,6 @@ def get_colorspace_name():
     
     ociopath = os.getenv('OCIO', None)
     if ociopath is None:
-        if 'RFB_USE_BLENDER_OCIO' not in os.environ:
-            return ''
         ociopath = get_blender_ocio_config()
     if ColorManager:
         clrmgr.update(ociopath)
